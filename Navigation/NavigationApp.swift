c369ae37c24d97f19377b7fad2cf7fac4ae6cf57
Zoe Pan <zoe.yi.pan@gmail.com>
2025-08-07 10:02:41 -0700

    Initial Commit

---
+++ Navigation/NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationApp.swift
+//  Navigation
+//
+//  Created by Scholar on 8/7/25.
+//
+
+import SwiftUI
+
+@main
+struct NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
